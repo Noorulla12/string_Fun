f99e683a6983bde2d6579435dfaf59cf89e89c7d
rupan <rupan@SSSOLVEM2800>
2024-07-01 12:41:55 +0530

    Add project files.

---
+++ string_Fun/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace stringFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Noorulla";
+            var lastName = "Rupanagudi";
+
+             Console.WriteLine("My name is " + firstName + " " + lastName);
+
+             Console.WriteLine(string.Format("{0} {1} ",firstName, lastName));
+
+            var names = new string[] { "Noor", "Rahim", "Baari" };
+
+            var formattedNames = string.Join(", ", names);
+
+            Console.WriteLine(formattedNames);
+
+            var text =@"Hi noor
+look into the following paths
+C:\projects\project1
+C:\folder1\folder2";
+
+            Console.WriteLine(text);
+
+
+
+
+
+
+        }
+    }
+
+}
